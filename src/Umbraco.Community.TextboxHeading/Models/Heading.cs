f8a864a5ed9b5238cd622c32408a95d3dc349dc8
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2025-07-09 15:43:42 +0200

    Add Value Converter

---
+++ src/Umbraco.Community.TextboxHeading/Models/Heading.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using Umbraco.Community.TextboxHeading.Enums;
+
+namespace Umbraco.Community.TextboxHeading.Models;
+public class Heading
+{
+    [JsonPropertyName("text")]
+    public string? Text { get; set; }
+
+    [JsonPropertyName("size")]
+    public HtmlHeading Size { get; set; }
+}
