363c8cb707ccc3205e2926d57d7e6863697fead5
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2025-07-10 14:57:06 +0200

    Add TagHelper

---
+++ src/Umbraco.Community.TextboxHeading/TagHelpers/HeadingTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Umbraco.Community.TextboxHeading.Models;
+
+namespace Umbraco.Community.TextboxHeading.TagHelpers;
+
+[HtmlTargetElement("umb-community-heading")]
+public class HeadingTagHelper : TagHelper
+{
+    public Heading? Heading { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        if (Heading == null)
+        {
+            return;
+        }
+
+        var tagName = Heading.Size.ToString().ToLower();
+        output.TagName = tagName;
+        output.TagMode = TagMode.StartTagAndEndTag;
+        output.Content.SetContent(Heading.Text);
+    }
+}
