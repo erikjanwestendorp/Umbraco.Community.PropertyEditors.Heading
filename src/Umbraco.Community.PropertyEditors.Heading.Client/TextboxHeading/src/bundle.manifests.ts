c9508cc995a2e1605524ede24426b9e950a7d814
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2025-09-09 15:18:23 +0200

    Delete ufm filter (prefix)

--- src/Umbraco.Community.PropertyEditors.Heading.Client/TextboxHeading/src/bundle.manifests.ts
+++ src/Umbraco.Community.PropertyEditors.Heading.Client/TextboxHeading/src/bundle.manifests.ts
@@ -1,11 +1,9 @@
 import { manifests as headingManifest } from './property-editors/heading/manifests.js';
 import { manifest as headingTypeManifest } from './property-editors/heading-size/manifests.js';
 import { manifest as headingValueManifest} from './ufm/components/manifest.js';
-import { manifest as headingValueFilterManifest} from './ufm/filters/manifest.js';
 
 export const manifests: Array<UmbExtensionManifest> = [
   ...headingManifest,
   headingTypeManifest,
-  headingValueManifest,
-  headingValueFilterManifest
+  headingValueManifest
 ]; 
